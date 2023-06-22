23a96fd0b20ff48e80e2653a5892325aebf84213
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-06-22 16:39:06 +0530

    retrieving sorted values from transaction table

--- src/main/java/HomePageAutomation.java
+++ src/main/java/HomePageAutomation.java
@@ -13,7 +13,30 @@ public class HomePageAutomation {
          WebDriver driver = new ChromeDriver();
 
          // Navigate to the login page
-         driver.get("https://sakshingp.github.io/assignment/login.html");
+         driver.get("https://sakshingp.github.io/assignment/home.html");
+
+         // Perform login using valid credentials
+         WebElement usernameInput = driver.findElement(By.id("username"));
+         WebElement passwordInput = driver.findElement(By.id("password"));
+         WebElement loginButton = driver.findElement(By.xpath("//button[contains(text(), 'Login')]"));
+
+         usernameInput.sendKeys("valid-username");
+         passwordInput.sendKeys("valid-password");
+         loginButton.click();
+
+         // Wait for the home page to load
+         // For waiting mechanism use Thread.sleep(2000)
+         try {
+            Thread.sleep(2000);
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+
+         // click on the AMOUNT header to sort the values
+         WebElement amountHeader = driver.findElement(By.xpath("//th[contains(text(), 'amount')]"));
+         amountHeader.click();
+
+         // Retreive the sorted values from the transaction table
     
     }
 }
