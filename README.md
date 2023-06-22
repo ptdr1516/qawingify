10dabb07ef40b9808e20f086e97ac5d3485bc9d4
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-06-22 18:18:46 +0530

    login and home page automated using different test cases

---
+++ README.md
@@ -0,0 +1,68 @@
+# Test Automation Scenarios
+
+This repository contains automation scripts for testing the Login Page and Home Page of the application. The scripts are implemented using Selenium WebDriver and Chrome Driver, written in Java, and are designed to cover various functional testing scenarios.
+
+## Testing Frameworks Used
+
+- Selenium WebDriver
+
+## Programming Language Used
+
+- Java
+
+
+## Test Automation Scenarios
+
+### Login Page
+
+#### Test Case 1: Verify Successful Login
+- Description: Verify that a user can successfully log in to the application using valid credentials.
+- Steps:
+  1. Launch the application URL.
+  2. Enter a valid username.
+  3. Enter a valid password.
+  4. Click on the "Login" button.
+
+- Note: Steps 2, 3 and 4 are automated.
+- Expected Result: The user should be logged in successfully and redirected to the Home Page.
+
+#### Test Case 2: Verify Invalid Login
+- Description: Verify that the application displays an error message when invalid credentials are used for login.
+- Steps:
+  1. Launch the application URL.
+  2. Enter an invalid username.
+  3. Enter an invalid password.
+  4. Click on the "Login" button.
+- Expected Result: The application should display an error message indicating that the login credentials are invalid.
+
+#### Test Case 3: Verify Empty Login Fields
+- Description: Verify that the application displays an error message when the login fields are submitted empty.
+- Steps:
+  1. Launch the application URL.
+  2. Leave the username field empty.
+  3. Leave the password field empty.
+  4. Click on the "Login" button.
+- Expected Result: The application should display an error message indicating that the login fields are required.
+
+#### Test Case 5: Verify Remember Me Functionality
+- Description: Verify that the "Remember Me" checkbox retains the user's login state.
+- Steps:
+  1. Launch the application URL.
+  2. Enter a valid username.
+  3. Enter a valid password.
+  4. Check the "Remember Me" checkbox.
+  5. Click on the "Login" button.
+  6. Log out of the application.
+  7. Launch the application URL again.
+- Expected Result: The application should remember the user's login state and automatically log them in without requiring credentials.
+
+### Home Page
+
+#### Test Case: Verify Sorting of Transaction Table by Amount
+- Description: Verify that the transaction table on the Home Page can be sorted by the Amount column.
+- Precondition: The user is logged in and on the Home Page.
+- Steps:
+  1. Click on the "Amount" header in the transaction table.
+- Expected Result: The values in the transaction table should be sorted in ascending or descending order based on the Amount column.'
+
+
--- src/main/java/LoginPageAutomation.java
+++ src/main/java/LoginPageAutomation.java
@@ -11,7 +11,7 @@ public class LoginPageAutomation {
         // create a new instance of ChromeDriver
         WebDriver driver = new ChromeDriver();
 
-        // Nvigate to the login page
+        // Navigate to the login page
         driver.get("https://sakshingp.github.io/assignment/login.html");
 
         // Perforing functional testing scenarios
@@ -21,13 +21,48 @@ public class LoginPageAutomation {
         WebElement loginButtton = driver.findElement(By.xpath("//button[@id='log-in']"));
 
         usernameInput.sendKeys("ptdr1516");
-        passwordInput.sendKeys("abc123@");
+        passwordInput.sendKeys("123456");
+        // Find and interact with the checkbox
+        WebElement rememberMeCheckbox = driver.findElement(By.xpath("//input[@type='checkbox']"));
+        rememberMeCheckbox.click();
         loginButtton.click();
 
-        // Testing login with empty credentials
-        usernameInput.clear();
-        passwordInput.clear();
-        loginButtton.click();
+         // Verify that the application displays an error message when invalid credentials are used for login
+         WebElement errorMessage = driver.findElement(By.id("error-message"));
+         String errorText = errorMessage.getText();
+         if (errorText.equals("Invalid username or password")) {
+             System.out.println("Test Case 1: Verify Invalid Login - Passed");
+         } else {
+             System.out.println("Test Case 1: Verify Invalid Login - Failed");
+         }
+ 
+         // Testing login with empty credentials
+         usernameInput.clear();
+         passwordInput.clear();
+         loginButtton.click();
+ 
+         // Verify that the application displays an error message when the login fields are submitted empty
+         String emptyFieldsErrorText = errorMessage.getText();
+         if (emptyFieldsErrorText.equals("Username and Password are required fields")) {
+             System.out.println("Test Case 2: Verify Empty Login Fields - Passed");
+         } else {
+             System.out.println("Test Case 2: Verify Empty Login Fields - Failed");
+         }
+ 
+         // Verify that the "Remember Me" checkbox retains the user's login state
+         usernameInput.sendKeys("ptdr1516");
+         passwordInput.sendKeys("123456");
+         
+         loginButtton.click();
+         driver.navigate().refresh();
+         
+         String loggedInUsername = usernameInput.getAttribute("value");
+         if (loggedInUsername.equals("ptdr1516")) {
+             System.out.println("Test Case 4: Verify Remember Me Functionality - Passed");
+         } else {
+             System.out.println("Test Case 4: Verify Remember Me Functionality - Failed");
+         }
+ 
 
         // close the browser
         driver.quit();
