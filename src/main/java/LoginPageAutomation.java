27bc204b8d96f4e47a8245644df74a4e1a9cf2e3
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-06-22 16:24:48 +0530

    login page automation

--- src/main/java/LoginPageAutomation.java
+++ src/main/java/LoginPageAutomation.java
@@ -1,3 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginPageAutomation {
-    
+    public static void main(String[] args) {
+        // set the path to the chrome driver executable
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Ptdr/Desktop/Interview Assessment/chromedriver_win32/chromedriver.exe");
+
+        // create a new instance of ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Nvigate to the login page
+        driver.get("https://sakshingp.github.io/assignment/login.html");
+
+        // Perforing functional testing scenarios
+        // Testing login with valid credentials
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        WebElement loginButtton = driver.findElement(By.xpath("//button[contains(text(), 'Login')]"));
+
+        usernameInput.sendKeys("ptdr1516");
+        passwordInput.sendKeys("abc123@");
+        loginButtton.click();
+
+        // Testing login with empty credentials
+        usernameInput.clear();
+        passwordInput.clear();
+        loginButtton.click();
+
+        // close the browser
+        driver.quit();
+    }
 }
